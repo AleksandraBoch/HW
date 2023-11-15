4b8667017c7e0f5331c9549bc19e3b1b6417500a
AleksandraBoch <shemaFoxy@gmail.com>
2023-11-15 14:23:44 +0300

    adding files

---
+++ s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -0,0 +1,23 @@
+import {UserType} from '../HW8'
+
+type ActionType =
+    | { type: 'sort'; payload: 'up' | 'down' }
+    | { type: 'check'; payload: number }
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
+    switch (action.type) {
+        case 'sort': { // by name
+
+            return action.payload==='up'
+                ? [...state].sort((a,b)=>a.name>b.name?1:-1)
+            :[...state].sort((a,b)=>a.name<b.name?1:-1)// need to fix
+        }
+        case 'check': {
+
+            return [...state].filter(u => u.age >= 18) // need to fix
+            return {...state}
+        }
+        default:
+            return state
+    }
+}
